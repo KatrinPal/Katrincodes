fe2a4e01261947202be51a6b5357a9f212747dab
KatrinPal <165931723+KatrinPal@users.noreply.github.com>
2024-04-03 19:21:46 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,33 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+    // temperature
+
+    var temp = -15;
+    if (temp <= 5.0) {
+      System.out.println("wear super warm clothes");
+    }
+
+    if (season.equals("winter")) {
+      System.out.println("Wear a warm jacket!");
+    }
+    else if (season.equals("spring")) {
+      System.out.println("Wear a T-shirt!");
+    }
+    else if (season.equals("summer")) {
+      System.out.println("Wear a swimming suite!");
+    }
+    else if (season.equals("autumn")) {
+      System.out.println("Wear a rain coat!");
+    }
+    else
+      {
+        Syste.out.println("Invalid season");
+      }
+    {}
+
+}
